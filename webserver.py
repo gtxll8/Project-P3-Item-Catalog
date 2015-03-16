00444515fb3e96a4e1c7f28ec064891de18faa8c
gtxll8 <george@chessborg.com>
2015-03-16 22:58:31 +0000

    update

--- webserver.py
+++ webserver.py
@@ -3,16 +3,28 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class webServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path.endswith("/hello"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message)
-            print message
-            return
-        else:
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                message = ""
+                message += "<html><body>Hello!</body></html>"
+                self.wfile.write(message)
+                print message
+                return
+
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                message = ""
+                message += "<html><body> &#161 Hola ! </body></html>"
+                self.wfile.write(message)
+                print message
+                return
+
+        except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
 
