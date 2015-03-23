3a25546ba8eefea892d9effe165d313b8d7264ec
gtxll8 <george@chessborg.com>
2015-03-23 21:34:51 +0000

    update

--- webserver.py
+++ webserver.py
@@ -12,7 +12,9 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>
+                          What would you like me to say?</h2><input name="message" type="text" >
+                          <input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -25,7 +27,9 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>
+                          What would you like me to say?</h2><input name="message" type="text" >
+                          <input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -47,7 +51,9 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>
+                      What would you like me to say?</h2><input name="message" type="text" >
+                      <input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
             self.wfile.write(output)
             print output
