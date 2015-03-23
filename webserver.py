874f78a0da8a98606abe3c26b1965b6d593fb82e
gtxll8 <george@chessborg.com>
2015-03-23 22:59:25 +0000

    update

--- webserver.py
+++ webserver.py
@@ -1,10 +1,44 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h2>Restaurants List:</h2></br>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    output += "<a href='#'>Edit</a></br>"
+                    output += "<a href='#'>Delete</a></br>"
+                    output += "</br>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -41,6 +75,7 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+
             self.send_response(301)
             self.end_headers()
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
