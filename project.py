dc36e9e57b06b64c259ba2d5d11e5a7d3378ad6b
gtxll8 <george@chessborg.com>
2015-03-30 23:16:02 +0100

    update

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask , render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -13,6 +14,7 @@ DBSession = sessionmaker(bind=engine)
 # A DBSession() instance establishes all conversations with the database
 session = DBSession()
 
+
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
@@ -21,18 +23,25 @@ def restaurantMenu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
-#Task 1: Create route for newMenuItem function here
+# Task 1: Create route for newMenuItem function here
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
-#Task 2: Create route for editMenuItem function here
+# Task 2: Create route for editMenuItem function here
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
-def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/edit/')
+def editMenuItem(restaurant_id, MenuID):
+        return "hello"
+
 
 #Task 3: Create a route for deleteMenuItem function here
 @app.route('/')
@@ -40,6 +49,7 @@ def editMenuItem(restaurant_id, menu_id):
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
+
 if __name__ == '__main__':
-  app.debug = True
-  app.run(host='0.0.0.0', port=5000)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
