7c69531af6b1935a89100ec4ed0b8ad194f07bf3
gtxll8 <george@chessborg.com>
2015-03-31 18:30:07 +0100

    Update

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 
 app = Flask(__name__)
 
@@ -27,13 +27,15 @@ def restaurantMenu(restaurant_id):
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
     if request.method == 'POST':
         newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("New menu item created !")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+        return render_template('newmenuitem.html', restaurant=restaurant, restaurant_id=restaurant_id)
 
 
 # Task 2: Create route for editMenuItem function here
@@ -54,11 +56,20 @@ def editMenuItem(restaurant_id, MenuID):
 
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
-def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:MenuID>/delete/', methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, MenuID):
+    deletedItem = session.query(MenuItem).filter_by(id=MenuID).one()
+    print MenuID
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template('deletemenuitem.html',restaurant_id=restaurant_id, MenuID=MenuID, item=deletedItem)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
