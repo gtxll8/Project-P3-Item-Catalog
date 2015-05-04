086eceda1a9bba3c1b63e7e06545738646a9d06d
gtxll8 <george@chessborg.com>
2015-05-04 10:40:22 +0100

    update

--- FinalProject3-Item-Catalog/project.py
+++ FinalProject3-Item-Catalog/project.py
@@ -47,6 +47,7 @@ class Anonymous(AnonymousUserMixin):
         self.name = 'Guest'
         self.social_id = 0
 
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -59,8 +60,8 @@ class UserNotFoundError(Exception):
 
 # Load users
 class User(UserMixin):
-    def __init__(self, id, name, social_id, active=True):
-        self.id = id
+    def __init__(self, user_id, name, social_id, active=True):
+        self.id = user_id
         self.name = name
         self.social_id = social_id
         self.active = active
@@ -89,7 +90,10 @@ def load_user(id):
     # 1. Fetch against the database a user by `id`
     # 2. Create a new object of `User` class and return it.
     u = session.query(Users).filter_by(id=id).first()
-    return User(u.id, u.name, u.social_id)
+    if u:
+        return User(u.id, u.name, u.social_id)
+    else:
+        return None
 
 
 # For a given file, return whether it's an allowed type or not
@@ -103,16 +107,17 @@ def allowed_file(filename):
 def login_test():
     return render_template('login_test.html')
 
+
 # Home route
 @app.route('/')
 def index():
     items = session.query(SaleItem).order_by(desc(SaleItem.id)).all()
     return render_template('index.html', items=items)
 
+
 # Login handler, must accept both GET and POST to be able to use OpenID.
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
 def login(provider_name):
-
     # We need response object for the WerkzeugAdapter.
     response = make_response()
 
@@ -127,8 +132,10 @@ def login(provider_name):
             result.user.update()
             # Check if this user is already in db
             user = session.query(Users).filter_by(social_id=result.user.id).first()
-            u = User(user.id, user.name, user.social_id)
+            print result.user.id
+
             if user:
+                u = User(user.id, user.name, user.social_id)
                 login_user(u)
                 if current_user.is_authenticated():
                     flash('You are already logged in.')
@@ -289,6 +296,27 @@ def deleteItem(user_id, item_id):
     else:
         # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
         return render_template('deleteitem.html', user_id=user_id, item_id=item_id, item=deletedItem)
+
+
+# Delete user account
+@app.route('/admin/<user_id>/delete/', methods=['GET', 'POST'])
+@login_required
+def deleteAccount(user_id):
+    deletedItems = session.query(SaleItem).filter_by(user_id=user_id).all()
+    if request.method == 'POST':
+        # delete the file from the upload folder too
+        for item in deletedItems:
+            if item.image_name and os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], item.image_name)):
+                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], item.image_name))
+        session.query(SaleItem).filter(user_id == user_id).delete()
+        session.query(Users).filter(Users.id == user_id).delete()
+        session.commit()
+        flash("Account deleteded !")
+        items = session.query(SaleItem).all()
+        render_template('index.html', items=items)
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template('deleteaccount.html', user_id=user_id)
 
 
 # Main seller's page
