a6b54772922ee2477af968a5c3ecb0bdcff471cc
gtxll8 <george@chessborg.com>
2015-05-10 18:55:01 +0100

    update

--- FinalProject3-Item-Catalog/database_setup.py
+++ FinalProject3-Item-Catalog/database_setup.py
@@ -19,8 +19,8 @@ class Users(Base):
 class SaleItem(Base):
     __tablename__ = 'sale_item'
     __searchable__ = ['description']
-    name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     description = Column(String(250))
     price = Column(String(8))
     image_name = Column(String(250))
@@ -29,6 +29,17 @@ class SaleItem(Base):
     user_name = Column(String(250), nullable=False)
     user = relationship(Users)
 
+    def __init__(self, name, description, price, image_name, category_name, user_id, user_name):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.image_name = image_name
+        self.category_name = category_name
+        self.user_id = user_id
+        self.user_name = user_name
+
+    def __repr__(self):
+        return '<sale_item %d>' % self.id
 
 engine = create_engine('sqlite:///salesite.db', echo=True)
 
