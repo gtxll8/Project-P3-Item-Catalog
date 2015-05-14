9bc737df9abaec3bc8d6b6cd7468e99b195d161b
gtxll8 <george@chessborg.com>
2015-05-14 12:04:09 +0100

    Update

---
+++ pg_config.sh
@@ -0,0 +1,9 @@
+apt-get -qqy update
+sudo apt-get upgrade
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+apt-get install nginx
+pip install gunicorn
+pip install authomatic
+pip install Flask-Login
+pip install Flask-WTF
