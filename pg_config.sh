ac5b0299122d454708f2e59cd06a5b5728ae8671
gtxll8 <george@chessborg.com>
2015-05-14 15:01:20 +0100

    Update

--- pg_config.sh
+++ pg_config.sh
@@ -3,8 +3,10 @@ sudo apt-get upgrade
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 apt-get -qqy install nginx
+apt-get -qqy install git
 pip install gunicorn
 pip install authomatic
 pip install Flask-Login
 pip install Flask-WTF
-pip install git+https://github.com/gtxll8/Project-P3-Item-Catalog.git@Project3
+pip install git
+git clone git+https://github.com/gtxll8/Project-P3-Item-Catalog.git
