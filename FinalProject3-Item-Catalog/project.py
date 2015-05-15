4c2a17b170896e50884a1142f138138f9cfa622d
gtxll8 <george@chessborg.com>
2015-05-15 09:53:13 +0100

    Update

--- FinalProject3-Item-Catalog/project.py
+++ FinalProject3-Item-Catalog/project.py
@@ -474,4 +474,4 @@ if __name__ == '__main__':
     handler.setLevel(logging.ERROR)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     app.logger.addHandler(handler)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
