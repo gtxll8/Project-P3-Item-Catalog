8aa6596a99c0a7c9ee9406e78b8780709da2d3cc
gtxll8 <george@chessborg.com>
2015-05-15 11:54:43 +0100

    Update

--- FinalProject3-Item-Catalog/pg_config.sh
+++ FinalProject3-Item-Catalog/pg_config.sh
@@ -2,5 +2,9 @@ apt-get -qqy update
 sudo apt-get upgrade
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
-apt-get install nginx
+apt-get -qqy install nginx
+apt-get -qqy install git
 pip install gunicorn
+pip install authomatic
+pip install Flask-Login
+pip install Flask-WTF
