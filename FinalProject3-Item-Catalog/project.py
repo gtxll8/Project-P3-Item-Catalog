fc0bbcfccfac54d633ffd05cf6735cebf050d569
gtxll8 <george@chessborg.com>
2015-05-15 15:03:48 +0100

    Update

--- FinalProject3-Item-Catalog/project.py
+++ FinalProject3-Item-Catalog/project.py
@@ -477,7 +477,6 @@ if __name__ == '__main__':
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     app.logger.addHandler(handler)
     # test-environment
-    # app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8080)
     # production
-    app.run()
+    # app.run()
